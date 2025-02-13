ac7c8b6c7e26cfe39b37b35669cddc8ec55af329
Araujo Marcos <marcosaraujo.tele@gmail.com>
2025-02-13 23:35:26 +0000

    Add Info

--- app/page.tsx
+++ app/page.tsx
@@ -9,6 +9,7 @@ import Image from "next/image";
 import { useRouter } from "next/navigation";
 import FileList from "@/components/FileListWorkbooks";
 import { Button } from "@/components/ui/button";
+import { CheckSquare } from "lucide-react";
 
 export default function Home() {
   const router = useRouter();
@@ -46,10 +47,80 @@ export default function Home() {
           <Input className="mx-2 bg-white" value={inputValue} onChange={(e) => setInputVlaue(e.target.value)} placeholder="Google Drive File Link"  />
           <Button disabled={!validLink} onClick={open}><BookOpen/>Open</Button>
         </div> */}
-        <div className="text-center m-auto py-[10vh]">
-          <Button variant={"outline"} onClick={() => router.push('/filepicker') } >Open Google Drive File</Button>
+        <div className="grid grid-cols-2 gap-4">
+          <div className={"col-span-2 md:col-span-1"}>
+            <div className="text-center p-10">
+              <Button className="text-2xl mt-5 p-5" variant={"outline"} onClick={() => router.push('/filepicker') } >Open Google Drive File</Button>
+            </div>
+            <div className=" m-auto text-center text-4xl py-10">Welcome to 2Workbook</div>
+            <p className="p-5 text-justify"><b>2Workbook</b> is a solution for streamlining the completion of educational materials in digital format. Designed for professionals and students in vocational training, it allows you to create, save, and access your question-and-answer workbooks directly from Google Drive, without the need for additional storage.</p>
+            <div className=" m-auto text-center text-xl pt-5">What is 2Workbook?</div>
+            <p className="p-5 text-justify"><b>2Workbook</b> is a web application integrated with Google Drive that enables users to open and save files in the .2workbook format. These files are structured to make completing mandatory training materials more efficient, helping you achieve your certification or qualification with ease.
+            </p>
+            <div className=" m-auto text-center text-xl pt-5">Key Features</div>
+            <div className="p-5">
+              <ul>
+                <li className="p-5 items-center flex">
+                  <div className="w-[60px]">
+                    <CheckSquare size={30} className="text-white"/>
+                  </div>
+                  <div className="w-[100%] text-justify">Seamless Google Drive Integration - No extra accounts or storage required.</div>
+                </li>
+                <li className="p-5 items-center flex">
+                  <div className="w-[60px]">
+                    <CheckSquare size={30} className="text-white"/>
+                  </div>
+                  <div className="w-[100%] text-justify">Fast & Simple Access - Open your files directly using “Open with” in Google Drive.</div>
+                </li>
+                <li className="p-5 items-center flex">
+                  <div className="w-[60px]">
+                    <CheckSquare size={30} className="text-white"/>
+                  </div>
+                  <div className="w-[100%] text-justify">User-Friendly Interface - Keep your study materials organized in an easy-to-use environment.</div>
+                </li>
+                <li className="p-5 items-center flex">
+                  <div className="w-[60px]">
+                    <CheckSquare size={30} className="text-white"/>
+                  </div>
+                  <div className="w-[100%] text-justify">Optimized for Easy Completion - Renders content in responsive HTML, making navigation and editing smooth.</div>
+                </li>
+                <li className="p-5 items-center flex">
+                  <div className="w-[60px]">
+                    <CheckSquare size={30} className="text-white"/>
+                  </div>
+                  <div className="w-[100%] text-justify">100% Cloud-Based - All your files remain securely stored in Google Drive.</div>
+                </li>
+              </ul>
+            </div>
+
+            <div className=" m-auto text-center text-xl pt-5">How It Works?</div>
+            <div className="p-5">
+              <ol className="px-10 list-decimal">
+                <li className="py-5">
+                  Seamless Google Drive Integration - No extra accounts or storage required.
+                </li>
+                <li className="py-5">
+                  Fast & Simple Access - Open your files directly using “Open with” in Google Drive.
+                </li>
+                <li className="py-5">
+                  User-Friendly Interface - Keep your study materials organized in an easy-to-use environment.
+                </li>
+                <li className="py-5">
+                  Optimized for Easy Completion - Renders content in responsive HTML, making navigation and editing smooth.
+                </li>
+                <li className="py-5">
+                  100% Cloud-Based - All your files remain securely stored in Google Drive.
+                </li>
+              </ol>
+            </div>
+          </div>
+          <div>
+            <div className="mt-10"></div>
+            <FileList/>
+          </div>
         </div>
-        <FileList/>
+        <div className="grid">
+        </div>
       </main>
       <footer className=" min-w-[70vw] row-start-3 flex justify-between gap-6 items-center ">
         <a
--- components/FileListWorkbooks.tsx
+++ components/FileListWorkbooks.tsx
@@ -3,8 +3,21 @@ import { useEffect, useState } from "react";
 import { Button } from "./ui/button";
 import { Download } from "lucide-react";
 import { useRouter } from "next/navigation";
-
+import { Input } from "./ui/input";
+const proQualCourses = [
+  "ProQual Level 3 NVQ Certificate in Occupational Health and Safety",
+  "ProQual Level 6 NVQ Diploma in Occupational Health and Safety Practice",
+  "ProQual Level 2 Diploma in Construction Operations",
+  "ProQual Level 6 NVQ Diploma in Construction Site Management",
+  "ProQual Level 3 Diploma in Business Administration",
+  "ProQual Level 5 NVQ Diploma in Management and Leadership",
+  "ProQual Level 3 Award in Assessing Competence in the Work Environment",
+  "ProQual Level 4 Award in the Internal Quality Assurance of Assessment Processes and Practice",
+  "ProQual Level 3 Award in Introduction to Crime Prevention",
+  "ProQual Level 5 Diploma in Crime Prevention & Designing Out Crime"
+];
 const FileList: React.FC = () => {
+  const [search, setSearch] = useState<string>('');
   const [files, setFiles] = useState<string[]>([]);
   const [error, setError] = useState<string | null>(null);
   const router = useRouter();
@@ -22,8 +35,9 @@ const FileList: React.FC = () => {
   }, []);
 
   return (
-    <div className="m-auto flex flex-col gap-2 text-center">
-      <div className="text-center"><b>Workbooks:</b></div>
+    <div className="m-auto bg-emerald-300 p-10 flex flex-col gap-2 text-center min-h-[80vh]">
+      <div className="text-start"><b>List of Workbooks:</b></div>
+      <Input className="bg-white" placeholder="Search" value={search} onChange={e => setSearch(e.target.value)} ></Input>
       {error ? (
         <p>{error}</p>
       ) : (
@@ -38,6 +52,19 @@ const FileList: React.FC = () => {
               </a>
             </div>
           ))}
+          <div className="text-start pt-5 "><b>SOON:</b></div>
+          {
+              proQualCourses.map((el, index) => (
+                <div className="flex flex-row" key={'workbook'+index}>
+                  <Button  style={{width: '75%'}} disabled>
+                    <span className="overflow-hidden text-ellipsis">{el}</span>
+                  </Button>
+                  <div style={{width: '25%'}}>
+                    <Button disabled><Download/></Button>
+                  </div>
+                </div>
+              ))
+            }
         </>
       )}
     </div>
